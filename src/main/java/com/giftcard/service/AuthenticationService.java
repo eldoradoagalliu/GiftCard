ffdc8a5407d4586f2f10ab6a95d1005ed4ec3e92
Eldorado Agalliu <eldoradoagalliu@gmail.com>
2025-03-04 15:56:08 +0100

    Update AuthenticationService

--- src/main/java/com/giftcard/service/AuthenticationService.java
+++ src/main/java/com/giftcard/service/AuthenticationService.java
@@ -1,16 +1,19 @@
 package com.giftcard.service;
 
-import com.giftcard.dto.ResponseDTO;
-import com.giftcard.model.AuthRequest;
-import com.giftcard.model.AuthResponse;
+import com.giftcard.model.dto.ResponseDTO;
+import com.giftcard.model.authentication.AuthenticationRequest;
+import com.giftcard.model.authentication.AuthenticationResponse;
 import com.giftcard.model.RegisterRequest;
 import com.giftcard.model.User;
 import com.giftcard.repository.UserRepository;
 import com.giftcard.util.DateUtils;
 import com.giftcard.util.JwtUtil;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -18,24 +21,46 @@ import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.concurrent.ExecutionException;
 
+import static com.giftcard.constant.ApplicationConstants.INVALID_CREDENTIALS;
+import static com.giftcard.constant.ApplicationConstants.SUCCESSFUL_REGISTRATION;
+import static com.giftcard.constant.ApplicationConstants.USER_ALREADY_EXISTS;
 import static com.giftcard.constant.ApplicationConstants.USER_CREATED_CODE;
 import static com.giftcard.constant.ApplicationConstants.USER_EXISTS_CODE;
-import static com.giftcard.enums.RoleEnum.CLIENT;
+import static com.giftcard.enums.Role.CLIENT;
 
 @Service
 @RequiredArgsConstructor
-public class AuthenticationService {
+public class AuthenticationService implements AuthenticationProvider {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
-    private final AuthenticationManager authenticationManager;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String email = authentication.getName();
+        String password = authentication.getCredentials().toString();
+        try {
+            User user = userRepository.getUser(email);
+            if (user == null || !passwordEncoder.matches(password, user.getPassword())) {
+                throw new BadCredentialsException(INVALID_CREDENTIALS);
+            }
+            return new UsernamePasswordAuthenticationToken(email, null, user.getAuthorities());
+        } catch (ExecutionException | InterruptedException e) {
+            throw new UsernameNotFoundException(e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+    }
 
     public ResponseDTO register(RegisterRequest request) throws ExecutionException, InterruptedException {
         if (userRepository.userExists(request.getEmail())) {
             return ResponseDTO.builder()
                     .code(USER_EXISTS_CODE)
-                    .message("User already exists. Try using a new email!")
+                    .message(USER_ALREADY_EXISTS)
                     .build();
         } else {
             var user = User.builder()
@@ -45,28 +70,34 @@ public class AuthenticationService {
                     .role(CLIENT.getRole())
                     .company(request.getCompany())
                     .phoneNumber(request.getPhoneNumber())
-                    .location(request.getPhoneNumber())
+                    .location(request.getLocation())
                     .createdAt(DateUtils.getLocalDateTime(LocalDateTime.now()))
                     .build();
+
             userRepository.createUser(user);
+
             return ResponseDTO.builder()
                     .code(USER_CREATED_CODE)
-                    .message("Successfully registered a user!")
+                    .message(SUCCESSFUL_REGISTRATION)
                     .build();
         }
     }
 
-    public AuthResponse authenticate(AuthRequest request) {
-        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
-        try {
-            var user = userRepository.getUser(request.getEmail());
-            var jwt = jwtUtil.generateToken(user);
-            return AuthResponse.builder()
-                    .role(user.getRole())
-                    .token(jwt)
-                    .build();
-        } catch (Exception e) {
-            throw new UsernameNotFoundException(e.getMessage());
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        Authentication authenticationResult = authenticate(new UsernamePasswordAuthenticationToken(request.getEmail(), request.getPassword()));
+        if (authenticationResult.isAuthenticated()) {
+            try {
+                var user = userRepository.getUser(request.getEmail());
+                var jwt = jwtUtil.generateToken(user);
+                return AuthenticationResponse.builder()
+                        .role(user.getRole())
+                        .token(jwt)
+                        .build();
+            } catch (Exception e) {
+                throw new UsernameNotFoundException(e.getMessage());
+            }
+        } else {
+            throw new BadCredentialsException(INVALID_CREDENTIALS);
         }
     }
 }
